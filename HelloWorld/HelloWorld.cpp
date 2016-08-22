d35248331e526a5cb915edd7087405a5bbba2db1
Aaron Andrews <aaron.andrews@ad.aie.edu>
2016-08-22 11:39:08 -0700

    Initial Push of files

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+// Aaron Andrews
+// aaron.andrews@students.aie.edu.au
+// 
+// Prints out "Hello World!"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	printf("Hello AIE, my name is Aaron Andrews!\n<Press enter to continue>");
+	getchar();
+	printf("I look fortward to creating awesome games with all of you!\n<Press enter to leave the chat>");
+	getchar();
+}
---
+++ Release/README.txt
@@ -0,0 +1,10 @@
+1. Open HelloWorld.exe
+2. Read the text
+3. Profit
+
+Credits
+_______
+
+
+Programmer,
+Aaron Andrews - aaron.andrews@students.aie.edu.au
